6e2cae3372a12c71db3672f7b4ccafa2c2ea8692
recon317 <bordnerryan@gmail.com>
2024-08-02 23:36:17 -0400

    changed the who shabang

--- Strings/strings.py
+++ Strings/strings.py
@@ -12,16 +12,27 @@ def read_file():
 
 
 bin_data = read_file()
-bin_data = bin_data[:10000]
+bin_data = bin_data
 
 
 def string_search(bin_data: List[int]):
+    last_char = ''
+    search_output = []
     alphabet = b"abcdefghijklmnopqrstuvwxyzABCDEFHIJKLMNOPQRSTUVWXYZ0123456789 "
     for i in bin_data:
         if i in alphabet:
-            if chr(i) == ' ':
-                print("")
-            print(chr(i), end='')
+            search_output.append(chr(i))
+        elif last_char in alphabet:
+            search_output.append('\n')
+        last_char = i
+    joined_output = ''.join(search_output)
+    split_output = joined_output.split('\n')
+    for item in split_output:
+        if len(item) >= 3:
+            print(item)
+
+
+
 
 
 string_search(bin_data)
